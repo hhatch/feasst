f7fa153edc7d5ab711b31a93ac371a678bcc5ad1
Harold Hatch <harold@hhatch.com>
2020-09-29 10:35:20 -0400

    Testing extmom on Gibbs, NumParticles better, serialize phase append

--- plugin/flat_histogram/tutorial/tutorial_8_trappe.sh
+++ plugin/flat_histogram/tutorial/tutorial_8_trappe.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-num_hours=5*24
+((num_hours=1))
+#((num_hours=5*24))
 num_procs=12
 ((num_procs_ext=2*$num_procs))
 
@@ -10,7 +11,7 @@ cat << _EOF_ > launch.cmd
 #!/bin/bash
 #SBATCH -n ${num_procs_ext}
 #SBATCH -N 1
-#SBATCH -t ${num_hours}:00
+#SBATCH -t ${num_hours}:00:00
 #SBATCH -o hostname_%j.out
 #SBATCH -e hostname_%j.out
 echo "Running on host \$(hostname)"
@@ -20,13 +21,27 @@ echo "ID is \$SLURM_JOB_ID"
 
 cd \$PWD
 
-python tutorial_8_trappe.py -p ../data.ethane -p ../data.ethene --num_hours $num_hours --num_procs $num_procs
+#python tutorial_8_trappe.py \
+#  --task \$SLURM_ARRAY_TASK_ID \
+#  -p ../data.ethane \
+#  -p ../data.ethene \
+#  --num_hours $num_hours \
+#  --num_procs $num_procs
+
+./tutorial \
+  --task \$SLURM_ARRAY_TASK_ID \
+  --particle0 ../data.ethane \
+  --particle1 ../data.ethene \
+  --num_hours $num_hours \
+  --num_procs $num_procs
 
 echo "Job is done"
 echo "Time is \$(date)"
 _EOF_
 
-sbatch launch.cmd
+sbatch --array=0-15%1 launch.cmd
 }
 
+cmake .
+make
 launch_node
