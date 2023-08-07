5e30e04739099a9dad81b5105d9e91d792e8fe60
Harold Hatch <harold.hatch@nist.gov>
2023-08-07 15:53:42 -0400

    capitalized global scope variables

--- tutorial/launch.py
+++ tutorial/launch.py
@@ -15,46 +15,46 @@ import matplotlib.pyplot as plt
 from pyfeasst import feasstio
 
 # Parse arguments from command line or change their default values.
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--fstprt', type=str, default='/feasst/forcefield/lj.fstprt',
+PARSER = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+PARSER.add_argument('--fstprt', type=str, default='/feasst/forcefield/lj.fstprt',
     help='FEASST particle definition')
-parser.add_argument('--beta', type=float, default=1./0.9, help='inverse temperature')
-parser.add_argument('--num_particles', type=int, default=500, help='number of particles')
-parser.add_argument('--rho_lower', type=float, default=1e-3, help='lowest number density')
-parser.add_argument('--rho_upper', type=float, default=9e-3, help='highest number density')
-parser.add_argument('--trials_per_iteration', type=int, default=int(1e5),
+PARSER.add_argument('--beta', type=float, default=1./0.9, help='inverse temperature')
+PARSER.add_argument('--num_particles', type=int, default=500, help='number of particles')
+PARSER.add_argument('--rho_lower', type=float, default=1e-3, help='lowest number density')
+PARSER.add_argument('--rho_upper', type=float, default=9e-3, help='highest number density')
+PARSER.add_argument('--trials_per_iteration', type=int, default=int(1e5),
     help='like cycles, but not necessary num_particles')
-parser.add_argument('--equilibration_iterations', type=int, default=int(1e1),
+PARSER.add_argument('--equilibration_iterations', type=int, default=int(1e1),
     help='number of iterations for equilibraiton')
-parser.add_argument('--production_iterations', type=int, default=int(1e1),
+PARSER.add_argument('--production_iterations', type=int, default=int(1e1),
     help='number of iterations for production')
-parser.add_argument('--hours_checkpoint', type=float, default=0.1, help='hours per checkpoint')
-parser.add_argument('--hours_terminate', type=float, default=0.1, help='hours until termination')
-parser.add_argument('--num_procs', type=int, default=5, help='number of processors')
-parser.add_argument('--prefix', type=str, default='lj', help='prefix for all output file names')
-parser.add_argument('--run_type', '-r', type=int, default=0,
+PARSER.add_argument('--hours_checkpoint', type=float, default=0.1, help='hours per checkpoint')
+PARSER.add_argument('--hours_terminate', type=float, default=0.1, help='hours until termination')
+PARSER.add_argument('--num_procs', type=int, default=5, help='number of processors')
+PARSER.add_argument('--prefix', type=str, default='lj', help='prefix for all output file names')
+PARSER.add_argument('--run_type', '-r', type=int, default=0,
     help='0: run, 1: submit to queue, 2: post-process')
-parser.add_argument('--seed', type=int, default=-1,
+PARSER.add_argument('--seed', type=int, default=-1,
     help='Random number generator seed. If -1, assign random seed to each sim.')
-parser.add_argument('--max_restarts', type=int, default=10, help='Number of SLURM restarts')
-parser.add_argument('--num_nodes', type=int, default=1, help='Number of SLURM nodes')
-parser.add_argument('--node', type=int, default=0, help='node ID')
-parser.add_argument('--slurm_id', type=int, default=-1, help='If != -1, read args from file')
-parser.add_argument('--slurm_task', type=int, default=0, help='If > 0, restart from checkpoint')
+PARSER.add_argument('--max_restarts', type=int, default=10, help='Number of SLURM restarts')
+PARSER.add_argument('--num_nodes', type=int, default=1, help='Number of SLURM nodes')
+PARSER.add_argument('--node', type=int, default=0, help='node ID')
+PARSER.add_argument('--slurm_id', type=int, default=-1, help='If != -1, read args from file')
+PARSER.add_argument('--slurm_task', type=int, default=0, help='If > 0, restart from checkpoint')
 
 # Convert arguments into a parameter dictionary, and add argument-dependent parameters.
 # Define sim-dependent parameters in run(sim, ...), with sim integer range of [0, num_sims-1].
-args, unknownargs = parser.parse_known_args()
-assert len(unknownargs) == 0, 'An unknown argument was included: '+str(unknownargs)
-params = vars(args)
-params['script'] = __file__
-params['sim_id_file'] = params['prefix']+ "_sim_ids.txt"
-params['minutes'] = int(params['hours_terminate']*60) # minutes used for SLURM queue time
-params['hours_terminate'] = 0.99*params['hours_terminate'] - 0.0333 # terminate before SLURM
-params['num_sims'] = params['num_nodes']*params['num_procs']
-params['rhos'] = np.linspace(params['rho_lower'], params['rho_upper'], num=params['num_sims'])
-params['cubic_box_lengths'] = np.power(params['num_particles']/params['rhos'], 1./3.).tolist()
-params['rhos'] = params['rhos'].tolist()
+ARGS, UNKNOWN_ARGS = PARSER.parse_known_args()
+assert len(UNKNOWN_ARGS) == 0, 'An unknown argument was included: '+str(UNKNOWN_ARGS)
+PARAMS = vars(ARGS)
+PARAMS['script'] = __file__
+PARAMS['sim_id_file'] = PARAMS['prefix']+ "_sim_ids.txt"
+PARAMS['minutes'] = int(PARAMS['hours_terminate']*60) # minutes used for SLURM queue time
+PARAMS['hours_terminate'] = 0.99*PARAMS['hours_terminate'] - 0.0333 # terminate before SLURM
+PARAMS['num_sims'] = PARAMS['num_nodes']*PARAMS['num_procs']
+PARAMS['rhos'] = np.linspace(PARAMS['rho_lower'], PARAMS['rho_upper'], num=PARAMS['num_sims'])
+PARAMS['cubic_box_lengths'] = np.power(PARAMS['num_particles']/PARAMS['rhos'], 1./3.).tolist()
+PARAMS['rhos'] = PARAMS['rhos'].tolist()
 
 def write_feasst_script(params, file_name):
     """ Write fst script for a single simulation with keys of params {} enclosed. """
@@ -144,7 +144,7 @@ class PostProcess(unittest.TestCase):
 
 if __name__ == '__main__':
     feasstio.run_simulations(run_function=run,
-                             params=params,
+                             params=PARAMS,
                              run_type=args.run_type,
                              slurm_id=args.slurm_id,
                              slurm_task=args.slurm_task)
