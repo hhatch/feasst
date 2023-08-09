8f3c3d64af0eb6ac399e8e11a0de466dfe3b6d1d
Harold Hatch <harold.hatch@nist.gov>
2023-08-09 14:58:07 -0400

    lint fh 10

--- plugin/flat_histogram/tutorial/launch_10_lj_wltm_t0.7.py
+++ plugin/flat_histogram/tutorial/launch_10_lj_wltm_t0.7.py
@@ -11,78 +11,80 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from pyfeasst import feasstio
+from pyfeasst import macrostate_distribution
+from pyfeasst import multistate_accumulator
 
 # Parse arguments from command line or change their default values.
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--feasst_install', type=str, default=os.path.expanduser('~')+'/feasst/build/',
-    help='FEASST install directory (e.g., the path to build)')
-parser.add_argument('--fstprt', type=str, default='/feasst/forcefield/lj.fstprt',
-    help='FEASST particle definition')
-parser.add_argument('--beta', type=float, default=1./0.7, help='inverse temperature')
-parser.add_argument('--mu', type=float, default=-4.1603632, help='chemical potential')
-parser.add_argument('--mu_init', type=float, default=10, help='initial chemical potential')
-parser.add_argument('--num_particles', type=int, default=475, help='number of particles')
-parser.add_argument('--num_particles_first_node', type=int, default=375,
-  help='number of particles in the first node')
-parser.add_argument('--cubic_box_length', type=float, default=8,
-    help='cubic periodic boundary length')
-parser.add_argument('--dccb_cut', type=float, default=1,
-    help='dual-cut configurational bias cutoff')
-parser.add_argument('--trials_per_iteration', type=int, default=int(1e6),
-    help='like cycles, but not necessary num_particles')
-parser.add_argument('--equilibration_iterations', type=int, default=0,
-    help='number of iterations for equilibraiton')
-parser.add_argument('--hours_checkpoint', type=float, default=0.1, help='hours per checkpoint')
-parser.add_argument('--hours_terminate', type=float, default=5*24, help='hours until termination')
-parser.add_argument('--procs_per_node', type=int, default=32, help='number of processors')
-parser.add_argument('--prefix', type=str, default='lj_lowt', help='prefix for all output file names')
-parser.add_argument('--run_type', '-r', type=int, default=0,
-    help='0: run, 1: submit to queue, 2: post-process')
-parser.add_argument('--seed', type=int, default=-1,
-    help='Random number generator seed. If -1, assign random seed to each sim.')
-parser.add_argument('--max_restarts', type=int, default=10, help='Number of restarts in queue')
-parser.add_argument('--num_nodes', type=int, default=2, help='Number of nodes in queue')
-parser.add_argument('--scratch', type=str, default=None,
-    help='Optionally write scheduled job to scratch/logname/jobid.')
-parser.add_argument('--node', type=int, default=0, help='node ID')
-parser.add_argument('--queue_id', type=int, default=-1, help='If != -1, read args from file')
-parser.add_argument('--queue_task', type=int, default=0, help='If > 0, restart from checkpoint')
+PARSER = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+PARSER.add_argument('--feasst_install', type=str, default=os.path.expanduser('~')+'/feasst/build/',
+                    help='FEASST install directory (e.g., the path to build)')
+PARSER.add_argument('--fstprt', type=str, default='/feasst/forcefield/lj.fstprt',
+                    help='FEASST particle definition')
+PARSER.add_argument('--beta', type=float, default=1./0.7, help='inverse temperature')
+PARSER.add_argument('--mu', type=float, default=-4.1603632, help='chemical potential')
+PARSER.add_argument('--mu_init', type=float, default=10, help='initial chemical potential')
+PARSER.add_argument('--num_particles', type=int, default=475, help='number of particles')
+PARSER.add_argument('--num_particles_first_node', type=int, default=375,
+                    help='number of particles in the first node')
+PARSER.add_argument('--cubic_box_length', type=float, default=8,
+                    help='cubic periodic boundary length')
+PARSER.add_argument('--dccb_cut', type=float, default=1,
+                    help='dual-cut configurational bias cutoff')
+PARSER.add_argument('--trials_per_iteration', type=int, default=int(1e6),
+                    help='like cycles, but not necessary num_particles')
+PARSER.add_argument('--equilibration_iterations', type=int, default=0,
+                    help='number of iterations for equilibraiton')
+PARSER.add_argument('--hours_checkpoint', type=float, default=0.1, help='hours per checkpoint')
+PARSER.add_argument('--hours_terminate', type=float, default=5*24, help='hours until termination')
+PARSER.add_argument('--procs_per_node', type=int, default=32, help='number of processors')
+PARSER.add_argument('--prefix', type=str, default='lj_lowt', help='prefix for output file names')
+PARSER.add_argument('--run_type', '-r', type=int, default=0,
+                    help='0: run, 1: submit to queue, 2: post-process')
+PARSER.add_argument('--seed', type=int, default=-1,
+                    help='Random number generator seed. If -1, assign random seed to each sim.')
+PARSER.add_argument('--max_restarts', type=int, default=10, help='Number of restarts in queue')
+PARSER.add_argument('--num_nodes', type=int, default=2, help='Number of nodes in queue')
+PARSER.add_argument('--scratch', type=str, default=None,
+                    help='Optionally write scheduled job to scratch/logname/jobid.')
+PARSER.add_argument('--node', type=int, default=0, help='node ID')
+PARSER.add_argument('--queue_id', type=int, default=-1, help='If != -1, read args from file')
+PARSER.add_argument('--queue_task', type=int, default=0, help='If > 0, restart from checkpoint')
 
 # Convert arguments into a parameter dictionary, and add argument-dependent parameters.
-args, unknown_args = parser.parse_known_args()
-assert len(unknown_args) == 0, 'An unknown argument was included: '+str(unknown_args)
-params = vars(args)
-params['script'] = __file__
-params['sim_id_file'] = params['prefix']+ '_sim_ids.txt'
-params['minutes'] = int(params['hours_terminate']*60) # minutes allocated on queue
-params['hours_terminate'] = 0.99*params['hours_terminate'] - 0.0333 # terminate FEASST before SLURM
-params['hours_terminate'] *= params['procs_per_node'] # real time -> cpu time
-params['hours_checkpoint'] *= params['procs_per_node']
-params['num_sims'] = params['num_nodes']
-params['procs_per_sim'] = params['procs_per_node']
-params['dccb_cut'] = params['cubic_box_length']/int(params['cubic_box_length']/params['dccb_cut'])
+ARGS, UNKNOWN_ARGS = PARSER.parse_known_args()
+assert len(UNKNOWN_ARGS) == 0, 'An unknown argument was included: '+str(UNKNOWN_ARGS)
+PARAMS = vars(ARGS)
+PARAMS['script'] = __file__
+PARAMS['sim_id_file'] = PARAMS['prefix']+ '_sim_ids.txt'
+PARAMS['minutes'] = int(PARAMS['hours_terminate']*60) # minutes allocated on queue
+PARAMS['hours_terminate'] = 0.99*PARAMS['hours_terminate'] - 0.0333 # terminate FEASST before SLURM
+PARAMS['hours_terminate'] *= PARAMS['procs_per_node'] # real time -> cpu time
+PARAMS['hours_checkpoint'] *= PARAMS['procs_per_node']
+PARAMS['num_sims'] = PARAMS['num_nodes']
+PARAMS['procs_per_sim'] = PARAMS['procs_per_node']
+PARAMS['dccb_cut'] = PARAMS['cubic_box_length']/int(PARAMS['cubic_box_length']/PARAMS['dccb_cut'])
 def sim_node_dependent_params(params):
     """ Define parameters that are dependent on the sim or node. """
     if params['node'] == 0:
-        params['min_particles']=0
-        params['max_particles']=params['num_particles_first_node']
-        params['gce_trial']='TrialTransfer weight 2 particle_type 0\nTrialTransferAVB weight 0.2 particle_type 0'
-        params['lj_potential']='Potential EnergyMap EnergyMapNeighborCriteria neighbor_index 0 Model LennardJones'
-        params['ref_potential']=''
-        params['avb_trials']='TrialAVB2 weight 0.1 particle_type 0\nTrialAVB4 weight 0.1 particle_type 0'
-        params['min_sweeps']=200
-        params['window_alpha']=2
-        params['min_window_size']=5
+        params['min_particles'] = 0
+        params['max_particles'] = params['num_particles_first_node']
+        params['gce_trial'] = 'TrialTransfer weight 2 particle_type 0\nTrialTransferAVB weight 0.2 particle_type 0'
+        params['lj_potential'] = 'Potential EnergyMap EnergyMapNeighborCriteria neighbor_index 0 Model LennardJones'
+        params['ref_potential'] = ''
+        params['avb_trials'] = 'TrialAVB2 weight 0.1 particle_type 0\nTrialAVB4 weight 0.1 particle_type 0'
+        params['min_sweeps'] = 200
+        params['window_alpha'] = 2
+        params['min_window_size'] = 5
     elif params['node'] == 1:
-        params['min_particles']=params['num_particles_first_node']
-        params['max_particles']=params['num_particles']
+        params['min_particles'] = params['num_particles_first_node']
+        params['max_particles'] = params['num_particles']
         params['gce_trial'] = 'TrialTransfer weight 2 particle_type 0 reference_index 0 num_steps 10'
-        params['lj_potential']='Potential Model LennardJones'
-        params['ref_potential']="""RefPotential Model LennardJones cutoff {dccb_cut} VisitModel VisitModelCell min_length {dccb_cut}""".format(**params)
-        params['avb_trials']=''
-        params['min_sweeps']=20
-        params['window_alpha']=1
-        params['min_window_size']=3
+        params['lj_potential'] = 'Potential Model LennardJones'
+        params['ref_potential'] = """RefPotential Model LennardJones cutoff {dccb_cut} VisitModel VisitModelCell min_length {dccb_cut}""".format(**params)
+        params['avb_trials'] = ''
+        params['min_sweeps'] = 20
+        params['window_alpha'] = 1
+        params['min_window_size'] = 3
 
 def write_feasst_script(params, file_name):
     """ Write fst script for a single simulation with keys of params {} enclosed. """
@@ -130,11 +132,7 @@ CriteriaWriter trials_per_write {trials_per_iteration} file_name {prefix}n{node}
 """.format(**params))
 
 def post_process(params):
-    # compare the lnpi with the srsw
-    import numpy as np
-    import pandas as pd
-    from pyfeasst import macrostate_distribution
-    from pyfeasst import multistate_accumulator
+    """ Compare the lnpi with the SRSW. """
     num_block = 32
     beta_mu_eq = list()
     rho_vapor = list()
@@ -152,25 +150,30 @@ def post_process(params):
         lnpi = macrostate_distribution.splice_files(prefix=params['prefix']+'n', suffix='_lnpi.txt',
                                                     ln_prob_header=ln_prob_header)
         lnpi.set_minimum_smoothing(30)
-        energy = multistate_accumulator.splice_by_node(prefix=params['prefix']+'n', suffix='_en.txt', num_nodes=params['num_nodes'])
+        energy = multistate_accumulator.splice_by_node(prefix=params['prefix']+'n',
+                                                       suffix='_en.txt',
+                                                       num_nodes=params['num_nodes'])
         lnpi.concat_dataframe(dataframe=energy, add_prefix='e_')
         delta_beta_mu = lnpi.equilibrium()
         beta_mu_eq.append(params['beta']*params['mu'] + delta_beta_mu)
         for index, phase in enumerate(lnpi.split()):
             n_gce = phase.average_macrostate()
             rho = n_gce/params['cubic_box_length']**3
-            en = phase.ensemble_average(energy_header)/n_gce
+            energy = phase.ensemble_average(energy_header)/n_gce
             if index == 0:
                 pressure.append(-phase.ln_prob()[0]/params['beta']/params['cubic_box_length']**3)
                 rho_vapor.append(rho)
-                en_vapor.append(en)
+                en_vapor.append(energy)
             else:
                 rho_liquid.append(rho)
-                en_liquid.append(en)
-    data = pd.DataFrame(data={'rho_vapor': rho_vapor, 'rho_liquid': rho_liquid, 'pressure': pressure, 'en_vapor': en_vapor, 'en_liquid': en_liquid, 'beta_mu_eq': beta_mu_eq})
+                en_liquid.append(energy)
+    data = pd.DataFrame(data={'rho_vapor': rho_vapor, 'rho_liquid': rho_liquid,
+                              'pressure': pressure, 'en_vapor': en_vapor, 'en_liquid': en_liquid,
+                              'beta_mu_eq': beta_mu_eq})
     data.to_csv(params['script']+'.csv')
     for col in data.columns:
-        print(col, data[col][0], '+/-', data[col][1:].std()/np.sqrt(len(data[col][1:])), data[col][1:].mean())
+        print(col, data[col][0], '+/-', data[col][1:].std()/np.sqrt(len(data[col][1:])),
+              data[col][1:].mean())
 
     # skip the following checks if temperature is not 0.7
     if np.abs(params['beta'] - 1./0.7) > 1e-5:
@@ -188,10 +191,10 @@ def post_process(params):
     # check lnpi
     lnpi = macrostate_distribution.splice_files(prefix=params['prefix']+'n', suffix='_lnpi.txt')
     srsw = pd.read_csv(params['feasst_install']+'../plugin/flat_histogram/test/data/stat070.csv')
-    df=pd.concat([lnpi.dataframe(), srsw], axis=1)
-    df['deltalnPI']=df.lnPI-df.lnPI.shift(1)
-    df.to_csv(params['prefix']+'_lnpi.csv')
-    diverged=df[df.deltalnPI-df.delta_ln_prob > z_factor*df.delta_ln_prob_stdev]
+    srsw = pd.concat([lnpi.dataframe(), srsw], axis=1)
+    srsw['deltalnPI'] = srsw.lnPI-srsw.lnPI.shift(1)
+    srsw.to_csv(params['prefix']+'_lnpi.csv')
+    diverged = srsw[srsw.deltalnPI-srsw.delta_ln_prob > z_factor*srsw.delta_ln_prob_stdev]
     print(diverged)
     assert len(diverged) == 0
 
@@ -205,9 +208,9 @@ def post_process(params):
     plt.savefig(params['prefix']+'_lnpi.png', bbox_inches='tight', transparent='True')
 
 if __name__ == '__main__':
-    feasstio.run_simulations(params=params,
+    feasstio.run_simulations(params=PARAMS,
                              sim_node_dependent_params=sim_node_dependent_params,
                              write_feasst_script=write_feasst_script,
                              post_process=post_process,
                              queue_function=feasstio.slurm_single_node,
-                             args=args)
+                             args=ARGS)
