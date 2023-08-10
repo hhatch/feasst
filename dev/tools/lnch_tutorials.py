e6310167db8c7550c52a683e2943c2591aaea3a2
Harold Hatch <harold.hatch@nist.gov>
2023-08-10 12:31:57 -0400

    updating lnch_tutorial.py for new file names

--- dev/tools/lnch_tutorials.py
+++ dev/tools/lnch_tutorials.py
@@ -22,7 +22,7 @@ for col in plugins.columns:
 
 # put all ids in nums list
 nums=list()
-for filename in Path('../').rglob('launch_ids.txt'):
+for filename in Path('../').rglob('_queue_ids.txt'):
     print(filename, filename.name, filename.parent)
     with open(filename) as f:
         nums.append(f.read().splitlines())
@@ -46,16 +46,16 @@ while not finished:
     time.sleep(60)
 
 # scrap job output for errors
-for filename in Path('../').rglob('hostname_*.out'):
+for filename in Path('../').rglob('_slurm_*.txt'):
     with cd.cd(filename.parent):
         subprocess.call("grep \"Err\" " + str(filename.name) + " >> launch_failures.txt", shell=True, executable='/bin/bash')
-        subprocess.call("grep \"Throw\" " + str(filename.name) + " >> launch_failures.txt", shell=True, executable='/bin/bash')
+        subprocess.call("grep \"Throw\" " + str(filename.name) + " | grep -v 'Terminating because Checkpoint has reached the user input' >> launch_failures.txt", shell=True, executable='/bin/bash')
         subprocess.call("grep \"No such file or directory\" " + str(filename.name) + " >> launch_failures.txt", shell=True, executable='/bin/bash')
     subprocess.call("grep \"Err\" " + str(filename) + " >> launch_failures.txt", shell=True, executable='/bin/bash')
     subprocess.call("grep \"Throw\" " + str(filename) + " >> launch_failures.txt", shell=True, executable='/bin/bash')
     subprocess.call("grep \"No such file or directory\" " + str(filename) + " >> launch_failures.txt", shell=True, executable='/bin/bash')
 
-for filename in Path('../').rglob('launch.log'):
+for filename in Path('../').rglob('_run.log'):
     with cd.cd(filename.parent):
         subprocess.call("grep \"Err\" " + str(filename.name) + " >> launch_failures.txt", shell=True, executable='/bin/bash')
         subprocess.call("grep \"FAILED\" " + str(filename.name) + " >> launch_failures.txt", shell=True, executable='/bin/bash')
