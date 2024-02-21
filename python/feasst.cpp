7b46c569a204bc172c997c98b966db22579a876e
Harold Hatch <harold.hatch@nist.gov>
2024-02-21 04:09:34 -0500

    example pybind11, needs undefined symbol fix

---
+++ python/feasst.cpp
@@ -0,0 +1,53 @@
+#include <pybind11/pybind11.h>
+#include "math/include/position.h"
+//#include "feasst/include/feasst.h"
+
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+int add(int i, int j) {
+    return i + j;
+}
+
+feasst::Position mc() {
+  return feasst::Position();
+}
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(_core, m) {
+    m.doc() = R"pbdoc(
+        Pybind11 example plugin
+        -----------------------
+
+        .. currentmodule:: scikit_build_example
+
+        .. autosummary::
+           :toctree: _generate
+
+           add
+           subtract
+    )pbdoc";
+
+    m.def("add", &add, R"pbdoc(
+        Add two numbers
+
+        Some other explanation about the add function.
+    )pbdoc");
+
+    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+        Subtract two numbers
+
+        Some other explanation about the subtract function.
+    )pbdoc");
+
+    m.def("mc", &mc, R"pbdoc(
+        Hi
+    )pbdoc");
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
