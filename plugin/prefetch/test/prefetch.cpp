7aaf0fc7e878e4fc6a2f034888241e07f5f4d0f0
Harold Hatch <harold.hatch@nist.gov>
2024-07-10 15:17:31 -0400

    Reapply "making system a unique ptr in monte carlo led to a lot of craziness"
    This reverts commit 753d6b160bf9b84033cb542f5b71b34dc78bbe23.

--- plugin/prefetch/test/prefetch.cpp
+++ plugin/prefetch/test/prefetch.cpp
@@ -124,8 +124,8 @@ void prefetch(System system, const int sync = 0) {
     }
   }
 
-  Prefetch mc2 = test_serialize(*mc);
-  EXPECT_EQ(1, mc2.trials_per_check());
+  auto mc2 = test_serialize_unique(*mc);
+  EXPECT_EQ(1, mc2->trials_per_check());
 }
 
 TEST(Prefetch, MUVT) {
