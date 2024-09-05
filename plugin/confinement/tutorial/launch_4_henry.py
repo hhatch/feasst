19efd96bb828634ef2f56ae65b824408c39681aa
Harold Hatch <harold.hatch@nist.gov>
2024-09-05 16:09:08 -0400

    doc

--- plugin/confinement/tutorial/launch_4_henry.py
+++ plugin/confinement/tutorial/launch_4_henry.py
@@ -1,7 +1,7 @@
 """
 This is a simple test of a henry coefficient calculation where a particle is inserted in a box
 where half the box leads to overlap (e.g., a hard wall potential).
-The <e^-{beta U}> term should be 1/2.
+The <e^-(beta U)> term should be 1/2.
 """
 
 import argparse
